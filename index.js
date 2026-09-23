b57def09e139ef29c47469680d3a360e45d3fb1d
Kalu Onuka Daniel <danielfrosh22@gmail.com>
2026-09-23 22:05:55 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+
+const name = "Daniel";
+const age = 20;
+const course = "JavaScript";
+const score = 50;
+
+
+const Atudent = {
+    name: "daniel",
+    age: 20,
+    course: "javaScript",
+    score: 50
+
+}
+console.log(Atudent.score);
+console.log(Atudent);
+console.log(Atudent.age);
+Atudent.email = "danoel@gmail.com"
+
+Atudent.age = 65;
+console.log(Atudent.age)
+Atudent.gender = "Male" 
+
+
+/* Array of object */
+
+const names = [
+    "daniel",
+    "israel",
+    "charles"
+]
+
+const students = [
+    {
+      name: "peter",
+      age: 50,
+      score: 30,
+    },
+
+    {
+       name: "Amarachi",
+      age: 20,
+      score: 90  
+    },
+
+    {
+        name: "dickson",
+      age: 44,
+      score: 70  
+    }
+];
+console.log(students[0]);
+console.log(students[2]);
+console.log(students);
+console.log(students[0].age);
+students[0].email = "daniel@gmail.com";
+students[1].score = 100;
+   
+
+
+
+
